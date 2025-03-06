ad33b96dd4f5a3cdc1c3ec625438a0f6ae7532fd
m1t4 <m1t4@users.noreply.github.com>
2025-03-06 11:34:50 +0200

    beging drawing player

--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
 from constants import *
-
+import player
 
 def main():
     print("Starting Asteroids!")
@@ -15,13 +14,20 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    p = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0))
+        p.draw(screen)
         pygame.display.flip()
+        dt = clock.tick() / 1000
 
 
 if __name__ == "__main__":
